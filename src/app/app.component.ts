fe057380ae74ee94eac2ac60ab3d5e4e8564766a
MauricioAburtol <xlr.2000@hotmail.com>
2024-01-17 13:46:44 -0600

    Routeo
    Rutas agregadas para desarrollo de paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Componenetes/header/header.component';
 import { FooterComponent } from './Componenetes/footer/footer.component';
+import { MainPageComponent } from './Paginas/main-page/main-page.component';
+import { ProfileComponent } from './Paginas/profile/profile.component';
+import { ShoppingBagPageComponent } from './Paginas/shopping-bag-page/shopping-bag-page.component';
+import { DetailProductPageComponent } from './Paginas/detail-product-page/detail-product-page.component';
+import { CategoryPageComponent } from './Paginas/category-page/category-page.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MainPageComponent, ProfileComponent, 
+    ShoppingBagPageComponent, DetailProductPageComponent, CategoryPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './Paginas/main-page/main-page.component';
+import { DetailProductPageComponent } from './Paginas/detail-product-page/detail-product-page.component';
+import { CategoryPageComponent } from './Paginas/category-page/category-page.component';
+import { ProfileComponent } from './Paginas/profile/profile.component';
+import { ShoppingBagPageComponent } from './Paginas/shopping-bag-page/shopping-bag-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/Inicio', pathMatch: 'full' },
+    { path: 'Inicio', component: MainPageComponent },
+    { path: 'Categorias', component: CategoryPageComponent },
+    {path: 'Producto', component: DetailProductPageComponent}, 
+    {path: 'Perfil', component: ProfileComponent},
+    {path: 'Bolsadecompra', component: ShoppingBagPageComponent}
+
+];
+
+export default routes;
