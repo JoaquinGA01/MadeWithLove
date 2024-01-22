2729e0521a94193e33de3a89f15757f0411c9fbe
MauricioAburtol <xlr.2000@hotmail.com>
2024-01-22 14:31:21 -0600

    Fix Body Error
    habia un error bien tonto en un archivo de body, listo

--- src/app/Paginas/main-page/components-main-page/new-products-display/new-products-display.component.ts
+++ src/app/Paginas/main-page/components-main-page/new-products-display/new-products-display.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-products-display',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './new-products-display.component.html',
   styleUrl: './new-products-display.component.css'
 })
 export class NewProductsDisplayComponent {
+
+  /*@ViewChild('productContainer') productContainer: Element;
+
+  products = [
+    // ... tus productos aquí
+  ];
+
+  scrollToProduct(index: number): void {
+    const containerScrollWidth = this.productContainer.nativeElement.scrollWidth;
+    const productWidth = containerScrollWidth / this.products.length;
+    this.productContainer.nativeElement.scrollTo({
+      left: productWidth * index,
+      behavior: 'smooth'
+    });
+  }*/
 
 }
--- src/app/Paginas/main-page/main-page.component.ts
+++ src/app/Paginas/main-page/main-page.component.ts
@@ -3,11 +3,12 @@ import { CarouselComponent } from '../../Componentes/carousel/carousel.component
 import { ProductButtonsMainpageComponent } from './components-main-page/product-buttons-mainpage/product-buttons-mainpage.component';
 import { NewSectionButtonsComponent } from './components-main-page/new-section-buttons/new-section-buttons.component';
 import { InfoButtonsComponent } from './components-main-page/info-buttons/info-buttons.component';
+import { NewProductsDisplayComponent } from './components-main-page/new-products-display/new-products-display.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CarouselComponent, ProductButtonsMainpageComponent, NewSectionButtonsComponent, InfoButtonsComponent],
+  imports: [CarouselComponent, ProductButtonsMainpageComponent, NewSectionButtonsComponent, InfoButtonsComponent, NewProductsDisplayComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
