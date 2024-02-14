e3d7316d226dc62208b7a5c71cffd365b6d3088e
MauricioAburtol <xlr.2000@hotmail.com>
2024-02-13 20:23:03 -0600

    Category Page changes and buttons mainpage
    Actualizacion de la pagina de categoria y correcciones en los botones principales

--- src/app/Paginas/category-page/category-page.component.ts
+++ src/app/Paginas/category-page/category-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterComponentComponent } from './components/filter-component/filter-component.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category-page',
   standalone: true,
-  imports: [FilterComponentComponent, CommonModule],
+  imports: [FilterComponentComponent, CommonModule, RouterLink],
   templateUrl: './category-page.component.html',
   styleUrl: './category-page.component.css'
 })
