5eca201386b98eabaeea9c5eb57f5f141141729b
MauricioAburtol <xlr.2000@hotmail.com>
2024-02-17 23:41:47 -0600

    Lógica y vista de pagina de producto
    Pagina de detalles del producto realizada con los cambios necesarios para que se muestren las imágenes con la vista y lógica necesaria, denle una caguamita al proyect manager, por wuapo. 🍺🍻

--- src/app/Paginas/detail-product-page/detail-product-page.component.ts
+++ src/app/Paginas/detail-product-page/detail-product-page.component.ts
@@ -9,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class DetailProductPageComponent {
 
+
+  featuredImage: string = './assets/Imagenes-detail-product-page/pastel1.png'; // Set the default featured image
+  setFeaturedImage(imageNumber: string): void {
+    switch (imageNumber) {
+      case 'image1':
+        this.featuredImage = './assets/Imagenes-detail-product-page/pastel1.png';
+        break;
+      case 'image2':
+        this.featuredImage = './assets/Imagenes-detail-product-page/pastel2.png';
+        break;
+      case 'image3':
+        this.featuredImage = './assets/Imagenes-detail-product-page/pastel3.png';
+        break;
+      default:
+        this.featuredImage = './assets/Imagenes-detail-product-page/pastel1.png';
+    }
+  }
+
+
+  count: number = 1;
+
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    if (this.count > 1) {
+      this.count--;
+    }
+  }
+
+  addToCart() {
+    console.log('Producto añadido al carrito:', this.count);
+    // Aquí podrías añadir la lógica para añadir el producto al carrito
+  }
 }
