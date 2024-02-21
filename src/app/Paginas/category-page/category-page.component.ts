29838b9bdfb6914de56d438a660d9946341c09a4
MauricioAburtol <xlr.2000@hotmail.com>
2024-02-21 15:54:26 -0600

    Category Page Adjust
    Ajustes de la pagina de categoría

--- src/app/Paginas/category-page/category-page.component.ts
+++ src/app/Paginas/category-page/category-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterComponentComponent } from './components/filter-component/filter-component.component';
 import { RouterLink } from '@angular/router';
+import { PanelOffersComponent } from '../../Componentes/panel-offers/panel-offers.component';
 
 @Component({
   selector: 'app-category-page',
   standalone: true,
-  imports: [FilterComponentComponent, CommonModule, RouterLink],
+  imports: [FilterComponentComponent, CommonModule, RouterLink, PanelOffersComponent],
   templateUrl: './category-page.component.html',
   styleUrl: './category-page.component.css'
 })
